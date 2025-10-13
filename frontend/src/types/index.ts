f5420019c44da4765419ce1c9ecf5e816c70cd08
Bisp1999 <peter@bissegger.ca>
2025-10-13 15:20:35 -0400

    fix(types): add role to User interface

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -25,6 +25,7 @@ export interface Tenant {
 export interface User {
   id: number;
   email: string;
+  role: 'user' | 'admin' | 'super_admin';
   is_admin: boolean;
   is_active: boolean;
   language: 'en' | 'fr';
